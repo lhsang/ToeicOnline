973099e7a03393efdbb32685750a83b54a950cae
lhsang <lehoangsangcpp@gmail.com>
2018-08-10 20:56:14 +0700

    first API - findAll

--- toeic-core-data/src/main/java/com/lhsang/core/data/daoimpl/AbstractDao.java
+++ toeic-core-data/src/main/java/com/lhsang/core/data/daoimpl/AbstractDao.java
@@ -2,11 +2,11 @@ package com.lhsang.core.data.daoimpl;
 
 import com.lhsang.core.common.utils.HibernateUtil;
 import com.lhsang.core.data.dao.GenericDao;
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
+import org.hibernate.*;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AbstractDao<ID extends Serializable, T> implements GenericDao<ID, T> {
@@ -15,10 +15,29 @@ public class AbstractDao<ID extends Serializable, T> implements GenericDao<ID, T
     public AbstractDao(){
         this.persistenceClass= (Class<T>) ((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[1];
     }
+    public String getPersistenceClassName(){
+        return persistenceClass.getSimpleName();
+    }
     protected Session getSession() {
         return HibernateUtil.getSessionFactory().openSession();
     }
     public List<T> findAll() {
-        return null;
+        List<T> list=new ArrayList<T>();
+        Transaction transaction=null;
+        transaction=getSession().beginTransaction();
+
+        //HQL
+        try {
+            StringBuilder sql=new StringBuilder("from ");
+            sql.append(this.getPersistenceClassName());
+            Query query=this.getSession().createQuery(sql.toString());
+            list=query.list();
+            transaction.commit();
+
+        }catch (HibernateException e) {
+            transaction.rollback();
+            throw e;
+        }
+        return list;
     }
 }
